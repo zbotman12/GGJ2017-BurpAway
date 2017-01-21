1b2dd93d91ca81829678a22780cc308264d660db
Ryan Kline <trk0002@uah.edu>
2017-01-20 23:42:03 -0600

    We're just getting started

---
+++ Assets/PlayerLaunch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
