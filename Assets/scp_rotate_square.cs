fe83dbb9ffad34e69fe14a4390e1cc4d5d056dd6
smw0002 <smw0002@alumni.uah.edu>
2017-01-21 20:41:40 -0600

    add buoy

---
+++ Assets/scp_rotate_square.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scp_rotate_square : MonoBehaviour {
+
+    public bool enable_rotate = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enable_rotate == true)
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
